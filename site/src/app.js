f008f60153b80af190cd9ca06aa2fdc114631d92
PieroJacinto <pierojacinto@gmail.com>
2022-11-14 18:44:29 -0300

    method override added, partials folder and files added, css structure added

--- site/src/app.js
+++ site/src/app.js
@@ -1,11 +1,19 @@
 require("dotenv").config({path:"../.env"});
 
-
+const express = require("express");
 const createApp = require("./config/create-app");
 const appConfig = require("./config/app-config");
 
+const methodOverride = require("method-override");
+
 const mainRouter = require("./routers/main-router");
 
+app.use(express.static(path.join(__dirname, "./public")));
+
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+app.use(methodOverride("_method"));
 
 // CREATE EXPRESS APP
 const app = createApp();
