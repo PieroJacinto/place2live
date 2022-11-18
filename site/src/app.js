ce68c5136c898521e674b0389d83a3fbeb02b602
Javier Alonso <jaalonsosilva@gmail.com>
2022-11-18 16:50:05 -0300

    home

--- site/src/app.js
+++ site/src/app.js
@@ -2,22 +2,22 @@ require("dotenv").config({path:"../.env"});
 const express = require("express")
 const createApp = require("./config/create-app");
 const appConfig = require("./config/app-config");
+const path = require ("path")
 
 // CREATE EXPRESS APP
 const app = createApp();
 appConfig.config(app);
-const path = require ("path")
 
 const methodOverride = require("method-override");
 
 const mainRouter = require("./routers/main-router");
 
-app.use(express.static(path.join(__dirname, "./public")));
+app.use(express.static(path.join(__dirname, "../public")));
 
-// app.use(express.urlencoded({ extended: false }));
-// app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
 
-// app.use(methodOverride("_method"));
+app.use(methodOverride("_method"));
 
 
 
