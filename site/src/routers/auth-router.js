7c5a6f16e34147463f6b6d5e28d3f631a75d2f50
PieroJacinto <pierojacinto@gmail.com>
2022-12-04 21:41:17 -0300

    register ejs names and tags fixed

--- site/src/routers/auth-router.js
+++ site/src/routers/auth-router.js
@@ -4,6 +4,7 @@ module.exports = router;
 
 const authController = require("../controllers/auth-controller");
 const loginValidations = require("../validators/login-validations").loginValidations
+
 router.get("/login",authController.viewLogin)
 router.post("/login",loginValidations, authController.login);
 
